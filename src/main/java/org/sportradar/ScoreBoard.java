a132dfd1d65d9a29d5b70e01f5509320dcdf1701
kwojtak <kasia.wojtak@gmail.com>
2025-07-14 21:08:28 +0200

    Add project skeleton

---
+++ src/main/java/org/sportradar/ScoreBoard.java
@@ -0,0 +1,6 @@
+package org.sportradar;
+
+public class ScoreBoard {
+
+
+}
---
+++ src/test/java/org/sportradar/ScoreBoardTest.java
@@ -0,0 +1,13 @@
+package org.sportradar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ScoreBoardTest {
+
+    @Test
+    void smokeTest(){
+        assertTrue(true);
+    }
+}
